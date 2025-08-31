1f0eea93598dc8413091e3c91c8cf4e13ddf68e3
RiandryDevelop <riandrydevs@gmail.com>
2025-08-31 17:22:02 -0400

    (feat/add) - Add menu
    - Añadiendo menu para el manejo de las escenas del juego

---
+++ Scripts/Global/Game.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Globals variables ( to settings the game )
+var playerHP = 100
+var Gold = 0  
+var transition_scene = false
+var current_scene = 'world'
+var climbing = false
+var going_Up = false
