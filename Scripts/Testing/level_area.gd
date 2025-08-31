08633818ad0cd9c84e6c6f10706c46540b2b4300
Meteoro9 <tolal.mazzone@gmail.com>
2025-08-31 18:09:35 -0300

    SFX_&_Player_animations
    añadí el efecto de sonido al eliminar una distracción. Faltan integrar (aunque están) los audios de explosión y disparo

--- Scripts/Testing/level_area.gd
+++ Scripts/Testing/level_area.gd
@@ -1,9 +1,16 @@
 extends Area2D
+class_name Elimination_Area_Behaviour
+
+@export var audio_exit: AudioStreamPlayer2D
 
 func _on_area_exited(body):
 	if body.name == "Distraction":
 		print("Se fue una distracción del área")
+		
+		audio_exit.play()
 		body.queue_free()
+		await audio_exit.finished
+		
 	else:
 		print("Qué chucha salió???!!!")
 	pass # Replace with function body.
